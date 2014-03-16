3adce97b76281aff9018c23066052a6e9418ba80
Esteban De La Fuente Rubio <esteban@delaf.cl>
2014-03-16 09:18:07 -0300

    Agregadas función para traducción y para hacer merge recursivo de arreglos

--- standard/basics.php
+++ standard/basics.php
@@ -24,7 +24,7 @@
 /**
  * @file basics.php
  * Archivo de funciones básicas para la aplicación
- * @version 2014-03-15
+ * @version 2014-03-16
  */
 
 /**
@@ -40,4 +40,67 @@ function debug ($var, $withtype = false) {
 	if ($withtype) var_dump($var);
 	else print_r($var);
 	if (class_exists('Dispatcher')) echo '</pre>',"\n";
+}
+
+/**
+ * Función para traducción de string singulares, en dominio master.
+ * @param string Texto que se desea traducir
+ * @param args Argumentos para reemplazar en el string, puede ser un arreglo o bien n argumentos a la función
+ * @return Texto traducido
+ * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
+ * @version 2014-03-16
+ */
+function __ ($string, $args = null) {
+	if (!is_array($args)) {
+		$args = array_slice(func_get_args(), 1);
+	}
+	return __d ('master', $string, $args);
+}
+
+/**
+ * Función para traducción de string singulares, eligiendo dominio.
+ * @param string Texto que se desea traducir
+ * @param args Argumentos para reemplazar en el string, puede ser un arreglo o bien n argumentos a la función
+ * @return Texto traducido
+ * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
+ * @version 2014-03-03
+ */
+function __d ($dominio, $string, $args = null) {
+	// si no hay argumentos solo se retorna el texto traducido
+	if (!$args) {
+		return I18n::translate($string, $dominio);
+	}
+	// si los argumentos no son un arreglo se obtiene arreglo a partir
+	// de los argumentos pasados a la función
+	if (!is_array($args)) {
+		$args = array_slice(func_get_args(), 2);
+	}
+	return vsprintf(I18n::translate($string, $dominio), $args);
+}
+
+/**
+ * Une dos o más arrays recursivamente
+ * @param array $array1
+ * @param array $array2
+ * @return array
+ * @author Daniel <daniel (at) danielsmedegaardbuus (dot) dk>
+ * @author Gabriel Sobrinho <gabriel (dot) sobrinho (at) gmail (dot) com>
+ */
+function array_merge_recursive_distinct ( array &$array1, array &$array2 ) {
+	$merged = $array1;
+	foreach ( $array2 as $key => &$value ) {
+		if (
+			is_array ( $value )
+			&& isset ( $merged [$key] )
+			&& is_array ( $merged [$key] )
+		) {
+			$merged [$key] = array_merge_recursive_distinct (
+				$merged [$key],
+				$value
+			);
+		} else {
+			$merged [$key] = $value;
+		}
+	}
+	return $merged;
 }
