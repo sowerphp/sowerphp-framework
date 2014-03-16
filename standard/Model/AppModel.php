608c69dceeaa30d8ec6df7edbd4609f47310611b
Esteban De La Fuente Rubio <esteban@delaf.cl>
2014-03-16 09:44:57 -0300

    Agregados archivos que faltaron cuando se hizo fork desde MiPaGiNa (modelos)

---
+++ standard/Model/AppModel.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * SowerPHP: Minimalist Framework for PHP
+ * Copyright (C) SowerPHP (http://sowerphp.org)
+ * 
+ * Este programa es software libre: usted puede redistribuirlo y/o
+ * modificarlo bajo los términos de la Licencia Pública General GNU
+ * publicada por la Fundación para el Software Libre, ya sea la versión
+ * 3 de la Licencia, o (a su elección) cualquier versión posterior de la
+ * misma.
+ * 
+ * Este programa se distribuye con la esperanza de que sea útil, pero
+ * SIN GARANTÍA ALGUNA; ni siquiera la garantía implícita
+ * MERCANTIL o de APTITUD PARA UN PROPÓSITO DETERMINADO.
+ * Consulte los detalles de la Licencia Pública General GNU para obtener
+ * una información más detallada.
+ * 
+ * Debería haber recibido una copia de la Licencia Pública General GNU
+ * junto a este programa.
+ * En caso contrario, consulte <http://www.gnu.org/licenses/gpl.html>.
+ */
+
+// Clase que extenderá esta clase
+App::uses('Model', 'Model');
+
+/**
+ * Clase abstracta para todos los modelos (clase sobreescribible)
+ * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
+ * @version 2012-10-29
+ */
+abstract class AppModel extends Model {
+}
+
+/**
+ * Clase abstracta para todos los modelos  (clase sobreescribible)
+ * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
+ * @version 2012-10-29
+ */
+abstract class AppModels extends Models {
+}
