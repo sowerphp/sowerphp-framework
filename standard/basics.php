51d6d5d3a983f6aeea3b03a7b25d441a03e1be8a
Esteban De La Fuente Rubio <esteban@delaf.cl>
2014-03-16 09:44:19 -0300

    Agregadas funciones: num, string2url, string_random y table2array

--- standard/basics.php
+++ standard/basics.php
@@ -104,3 +104,73 @@ function array_merge_recursive_distinct ( array &$array1, array &$array2 ) {
 	}
 	return $merged;
 }
+
+/**
+ * Función para formatear números
+ * @param n Número a formatear
+ * @param d Cantidad de decimales
+ * @return String Número formateado
+ * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
+ * @version 2012-10-19
+ */
+function num ($n, $d=0) {
+	return number_format($n, $d, ',', '.');
+}
+
+/**
+ * Convierte una cadena de texto "normal" a una del tipo url, ejemplo:
+ *   Cadena normal: Esto es un texto
+ *   Cadena convertida: esto-es-un-texto
+ * @param string String a convertir
+ * @param encoding Codificación del string
+ * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
+ * @version 2014-02-17
+ */
+function string2url ($string, $encoding = 'UTF-8') {
+	// tranformamos todo a minúsculas
+	$string = mb_strtolower($string, $encoding);
+	// rememplazamos carácteres especiales latinos
+	$find = array('á', 'é', 'í', 'ó', 'ú', 'ñ');
+	$repl = array('a', 'e', 'i', 'o', 'u', 'n');
+	$string = str_replace($find, $repl, $string);
+	// añadimos los guiones
+	$find = array(' ', '&', '\r\n', '\n', '+', '_');
+	$string = str_replace($find, '-', $string);
+	// eliminamos y reemplazamos otros caracteres especiales
+	$find = array('/[^a-z0-9\-<>]/', '/[\-]+/', '/<[^>]*>/');
+	$repl = array('', '-', '');
+	$string = preg_replace($find, $repl, $string);
+	unset($find, $repl);
+	return $string;
+}
+
+/**
+ * Función que genera un string de manera aleatoria
+ * @param length Tamaño del string que se desea generar
+ * @author http://www.lost-in-code.com/programming/php-code/php-random-string-with-numbers-and-letters
+ */
+function string_random ($length = 10) {
+	$characters = '0123456789';
+	$characters .= 'abcdefghijklmnopqrstuvwxyz';
+	$characters .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	$string = '';
+	for ($p = 0; $p < $length; $p++) {
+		$string .= $characters[mt_rand(0, strlen($characters)-1)];
+	}
+	return $string;
+}
+
+/**
+ * Convierte una tabla de Nx2 (N filas 2 columnas) a un arreglo asociativo
+ * @param table Tabla de Nx2 (N filas 2 columnas) que se quiere convertir
+ * @return Arreglo convertido a asociativo
+ * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
+ * @version 2014-02-24
+ */
+function table2array ($table) {
+	$array = array();
+	foreach($table as &$row) {
+		$array[array_shift($row)] = array_shift($row);
+	}
+	return $array;
+}
