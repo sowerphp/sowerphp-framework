ac211f1a8d0d4c5aeb3bacf737f9fc470f4d2658
Esteban De La Fuente Rubio <esteban@delaf.cl>
2014-12-11 23:24:51 -0300

    Se mueve controlador de contacto al core desde extensión general ya que casi siempre se querrá un formulario de contacto

---
+++ lib/sowerphp/core/Controller/Contacto.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * SowerPHP: Minimalist Framework for PHP
+ * Copyright (C) SowerPHP (http://sowerphp.org)
+ *
+ * Este programa es software libre: usted puede redistribuirlo y/o
+ * modificarlo bajo los términos de la Licencia Pública General GNU
+ * publicada por la Fundación para el Software Libre, ya sea la versión
+ * 3 de la Licencia, o (a su elección) cualquier versión posterior de la
+ * misma.
+ *
+ * Este programa se distribuye con la esperanza de que sea útil, pero
+ * SIN GARANTÍA ALGUNA; ni siquiera la garantía implícita
+ * MERCANTIL o de APTITUD PARA UN PROPÓSITO DETERMINADO.
+ * Consulte los detalles de la Licencia Pública General GNU para obtener
+ * una información más detallada.
+ *
+ * Debería haber recibido una copia de la Licencia Pública General GNU
+ * junto a este programa.
+ * En caso contrario, consulte <http://www.gnu.org/licenses/gpl.html>.
+ */
+
+namespace sowerphp\core;
+
+/**
+ * Controlador para página de contacto
+ * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
+ * @version 2014-12-10
+ */
+class Controller_Contacto extends \Controller_App
+{
+
+    /**
+     * Método para autorizar la carga de index en caso que hay autenticación
+     * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
+     * @version 2014-12-10
+     */
+    public function beforeFilter()
+    {
+        if (isset($this->Auth))
+            $this->Auth->allow('index');
+    }
+
+    /**
+     * Método que desplegará y procesará el formulario de contacto
+     * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
+     * @version 2014-12-10
+     */
+    public function index()
+    {
+        // si no hay datos para el envió del correo electrónico no
+        // permirir cargar página de contacto
+        if (Configure::read('email.default')===NULL) {
+            Model_Datasource_Session::message(
+                'Página de contacto deshabilitada', 'error'
+            );
+            $this->redirect('/');
+        }
+        // si se envió el formulario se procesa
+        if (!empty($_POST['nombre']) and !empty($_POST['correo']) and !empty($_POST['mensaje'])) {
+            $email = new Network_Email();
+            $email->replyTo($_POST['correo'], $_POST['nombre']);
+            $email->to(Configure::read('email.default.to'));
+            $email->subject('Contacto desde la web #'.date('YmdHis'));
+            $msg = $_POST['mensaje']."\n\n".'-- '."\n".$_POST['nombre']."\n".$_POST['correo'];
+            if (!is_array($email->send($msg))) {
+                Model_Datasource_Session::message(
+                    'Su mensaje ha sido enviado, se responderá a la brevedad.', 'ok'
+                );
+            } else {
+                Model_Datasource_Session::message(
+                    'Ha ocurrido un error al intentar enviar su mensaje, por favor intente nuevamente.', 'error'
+                );
+            }
+            $this->redirect('/contacto');
+        }
+    }
+
+}
