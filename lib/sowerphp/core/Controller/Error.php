e5849df7444ccae9d8f52a2c2344bf8e7ab440d8
Esteban De La Fuente Rubio <esteban@delaf.cl>
2015-04-26 11:27:26 -0300

    Siempre se asignan los datos de error a la vista que se renderizará (la vista decide si los muestra o no)

--- lib/sowerphp/core/Controller/Error.php
+++ lib/sowerphp/core/Controller/Error.php
@@ -38,22 +38,23 @@ class Controller_Error extends \Controller_App
      * Renderizar error
      * @param data Datos qye se deben pasar a la vista del error
      * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
-     * @version 2015-04-07
+     * @version 2015-04-26
      */
     public function display ($data)
     {
+        // agregar datos para la vista
+        $data['trace'] = str_replace(
+            [DIR_FRAMEWORK, DIR_WEBSITE],
+            ['DIR_FRAMEWORK', 'DIR_WEBSITE'],
+            $data['trace']
+        );
+        $this->set($data);
         // mostrar error exacto solo si se debe
-        if ($this->error_reporting) {
-            $data['trace'] = str_replace(
-                [DIR_FRAMEWORK, DIR_WEBSITE],
-                ['DIR_FRAMEWORK', 'DIR_WEBSITE'],
-                $data['trace']
-            );
-            $this->set($data);
+        if ($this->error_reporting)
             $this->render('Error/error_reporting');
-        }
         // mostrar error "genérico"
-        else $this->render('Error/silence');
+        else
+            $this->render('Error/silence');
         // enviar al cliente
         $this->response->status($data['code']);
         $this->response->send();
