50e1da38b45934e805e508b27d381a4e250468b1
Esteban De La Fuente Rubio <esteban@delaf.cl>
2018-10-15 19:14:13 -0300

    Se agrega favicon

--- lib/sowerphp/core/View/Layouts/harbor.php
+++ lib/sowerphp/core/View/Layouts/harbor.php
@@ -8,6 +8,7 @@ Layout: harbor (https://hackerthemes.com)
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title><?=$_header_title?></title>
+        <link rel="shortcut icon" href="<?=$_base?>/img/favicon.png" />
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
         <link href="https://fonts.googleapis.com/css?family=Oswald|Raleway" rel="stylesheet">
         <link rel="stylesheet" href="<?=$_base?>/layouts/harbor/bootstrap4-harbor.min.css">
