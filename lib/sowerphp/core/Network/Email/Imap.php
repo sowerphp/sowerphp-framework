e3dc350beb69ef9e1e187784c089eb5341216fc0
Esteban De La Fuente Rubio <esteban@delaf.cl>
2019-07-19 13:34:54 -0400

    Se deja con tag warning

--- lib/sowerphp/core/Network/Email/Imap.php
+++ lib/sowerphp/core/Network/Email/Imap.php
@@ -302,7 +302,7 @@ class Network_Email_Imap
             $message['attachments'][] = [
                 'name' => $filename,
                 'data' => $data,
-                'size' => !empty($p->bytes) ? $p->bytes : null, // a veces genera PHP Notice:  Undefined property: stdClass::$bytes
+                'size' => !empty($p->bytes) ? $p->bytes : null, // WARNING a veces genera PHP Notice:  Undefined property: stdClass::$bytes
                 'type' => (new \finfo(FILEINFO_MIME_TYPE))->buffer($data),
             ];
         }
