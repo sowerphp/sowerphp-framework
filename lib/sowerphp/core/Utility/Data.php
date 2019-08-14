622572b67677373e99e2868dc717bfc72f4dea76
Esteban De La Fuente Rubio <esteban@delaf.cl>
2019-08-14 15:17:01 -0400

    Se usa por defecto sodium para encriptar/desencriptar

--- lib/sowerphp/core/Utility/Data.php
+++ lib/sowerphp/core/Utility/Data.php
@@ -29,10 +29,40 @@ namespace sowerphp\core;
  *  - Sanitizar/limpiar datos
  *
  * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
- * @version 2017-02-28
+ * @version 2019-08-14
  */
 class Utility_Data
 {
+
+    protected static $default_crypt_method = 'sodium'; ///< método por defecto
+
+    /**
+     * Método que entrega el método de encriptación que se debe usar
+     * @param crypt_method Método forzado si se desea usar uno en específico
+     * @return String con el nombre del método de encriptación
+     * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
+     * @version 2019-08-14
+     */
+    protected static function getCryptMethod($crypt_method = null)
+    {
+        // buscar método de encriptación solicitado
+        if ($crypt_method) {
+            $crypt_method = $crypt_method;
+        } else {
+            $config_crypt_method = Configure::read('data.crypt.method');
+            if ($config_crypt_method) {
+                $crypt_method = $config_crypt_method;
+            } else {
+                $crypt_method = self::$default_crypt_method;
+            }
+        }
+        // verificar que el método solicitado exista
+        if (!method_exists('Utility_Data', $crypt_method.'_encrypt') or !method_exists('Utility_Data', $crypt_method.'_decrypt')) {
+            throw new \Exception(__('Método de encriptado %s no disponible en Utility_Data', $crypt_method));
+        }
+        // entregar método de encriptación
+        return $crypt_method;
+    }
 
     /**
      * Método que encripta un texto plano
@@ -40,27 +70,15 @@ class Utility_Data
      * @param key Clave a usar para encriptar
      * @return Texto encriptado en base64
      * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
-     * @version 2019-07-12
+     * @version 2019-08-14
      */
-    public static function encrypt($plaintext, $key = null)
+    public static function encrypt($plaintext, $key = null, $crypt_method = null)
     {
         if (!$key) {
             return base64_encode($plaintext);
         }
-        if (mb_strlen($key, '8bit') !== 32) {
-            throw new Exception('Se requiere una llave de 256 bits (32 caracteres si es llave ASCII)');
-        }
-        // usar mcrypt si está disponible
-        if (function_exists('mcrypt_encrypt')) {
-            $iv_size = @mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC);
-            $iv = @mcrypt_create_iv($iv_size, MCRYPT_RAND);
-            $ciphertext = @mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $plaintext, MCRYPT_MODE_CBC, $iv);
-            return base64_encode($iv.$ciphertext);
-        }
-        // mcrypt no está disponible
-        else {
-            throw new Exception('La extensión mcrypt de PHP no está disponible');
-        }
+        $method = self::getCryptMethod($crypt_method).'_encrypt';
+        return base64_encode(self::$method($plaintext, $key));
     }
 
     /**
@@ -69,32 +87,115 @@ class Utility_Data
      * @param key Clave a uar para desencriptar
      * @return Texto plano
      * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
-     * @version 2019-07-12
+     * @version 2019-08-14
      */
-    public static function decrypt($ciphertext_base64, $key = null)
+    public static function decrypt($ciphertext_base64, $key = null, $crypt_method = null)
     {
         if (empty($ciphertext_base64)) {
             return $ciphertext_base64;
         }
         $ciphertext_dec = base64_decode($ciphertext_base64);
+        if ($ciphertext_dec === false) {
+            throw new \Exception(__('Error al usar base64_decode() en el mensaje cifrado'));
+        }
         if (!$key) {
             return $ciphertext_dec;
         }
+        $method = self::getCryptMethod($crypt_method).'_decrypt';
+        return self::$method($ciphertext_dec, $key);
+    }
+
+    /**
+     * Método que encripta un texto plano usando sodium
+     * @link https://stackoverflow.com/a/52688846
+     * @author M_R_K (https://stackoverflow.com/users/1362713/m-r-k)
+     * @version 2018-10-07
+     */
+    private static function sodium_encrypt($plaintext, $key)
+    {
+        // verificar que exista la función de mcrypt
+        if (!function_exists('sodium_crypto_secretbox')) {
+            throw new \Exception(__('La extensión %s de PHP no está disponible', 'sodium'));
+        }
+        // encriptar
+        $nonce = random_bytes(SODIUM_CRYPTO_SECRETBOX_NONCEBYTES);
+        $ciphertext_dec = $nonce.sodium_crypto_secretbox($plaintext, $nonce, $key);
+        sodium_memzero($plaintext);
+        sodium_memzero($key);
+        return $ciphertext_dec;
+    }
+
+    /**
+     * Método que desencripta un texto encriptado usando sodium
+     * @link https://stackoverflow.com/a/52688846
+     * @author M_R_K (https://stackoverflow.com/users/1362713/m-r-k)
+     * @version 2018-10-07
+     */
+    private static function sodium_decrypt($ciphertext_dec, $key)
+    {
+        // verificar que exista la función de mcrypt
+        if (!function_exists('sodium_crypto_secretbox_open')) {
+            throw new \Exception(__('La extensión %s de PHP no está disponible', 'sodium'));
+        }
+        // desencriptar
+        if (mb_strlen($ciphertext_dec, '8bit') < (SODIUM_CRYPTO_SECRETBOX_NONCEBYTES + SODIUM_CRYPTO_SECRETBOX_MACBYTES)) {
+            throw new \Exception('El mensaje cifrado está truncado, no es válido');
+        }
+        $nonce = mb_substr($ciphertext_dec, 0, SODIUM_CRYPTO_SECRETBOX_NONCEBYTES, '8bit');
+        $ciphertext = mb_substr($ciphertext_dec, SODIUM_CRYPTO_SECRETBOX_NONCEBYTES, null, '8bit');
+        $plaintext = sodium_crypto_secretbox_open($ciphertext, $nonce, $key);
+        if ($plaintext === false) {
+            throw new \Exception('El mensaje fue manipulado después de ser encriptado, no es válido');
+        }
+        sodium_memzero($ciphertext);
+        sodium_memzero($key);
+        return $plaintext;
+    }
+
+    /**
+     * Método que encripta un texto plano usando mcrypt
+     * @deprecated https://www.php.net/manual/es/migration71.deprecated.php
+     * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
+     * @version 2019-08-14
+     */
+    private static function mcrypt_encrypt($plaintext, $key)
+    {
         if (mb_strlen($key, '8bit') !== 32) {
-            throw new Exception('Se requiere una llave de 256 bits (32 caracteres si es llave ASCII)');
+            throw new \Exception(__('Se requiere una llave de 256 bits (32 caracteres si es llave ASCII)'));
         }
-        // usar mcrypt si está disponible
-        if (function_exists('mcrypt_decrypt')) {
-            $iv_size = @mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC);
-            $iv_dec = substr($ciphertext_dec, 0, $iv_size);
-            $ciphertext_dec = substr($ciphertext_dec, $iv_size);
-            $plaintext_dec = @mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key, $ciphertext_dec, MCRYPT_MODE_CBC, $iv_dec);
-            return $plaintext_dec;
+        // verificar que exista la función de mcrypt
+        if (!function_exists('mcrypt_encrypt')) {
+            throw new \Exception(__('La extensión %s de PHP no está disponible', 'mcrypt'));
         }
-        // mcrypt no está disponible
-        else {
-            throw new Exception('La extensión mcrypt de PHP no está disponible');
+        // encriptar
+        $iv_size = @mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC);
+        $iv = @mcrypt_create_iv($iv_size, MCRYPT_RAND);
+        $ciphertext = @mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $key, $plaintext, MCRYPT_MODE_CBC, $iv);
+        $ciphertext_dec = $iv.$ciphertext;
+        return $ciphertext_dec;
+    }
+
+    /**
+     * Método que desencripta un texto encriptado usando mcrypt
+     * @deprecated https://www.php.net/manual/es/migration71.deprecated.php
+     * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
+     * @version 2019-08-14
+     */
+    private static function mcrypt_decrypt($ciphertext_dec, $key)
+    {
+        if (mb_strlen($key, '8bit') !== 32) {
+            throw new \Exception(__('Se requiere una llave de 256 bits (32 caracteres si es llave ASCII)'));
         }
+        // verificar que exista la función de mcrypt
+        if (!function_exists('mcrypt_decrypt')) {
+            throw new \Exception(__('La extensión %s de PHP no está disponible', 'mcrypt'));
+        }
+        // desencriptar
+        $iv_size = @mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC);
+        $iv_dec = substr($ciphertext_dec, 0, $iv_size);
+        $ciphertext_dec = substr($ciphertext_dec, $iv_size);
+        $plaintext = @mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key, $ciphertext_dec, MCRYPT_MODE_CBC, $iv_dec);
+        return $plaintext;
     }
 
     /**
