1f50be12f08289740a86f68a3702027442370fd3
Esteban De La Fuente Rubio <esteban@delaf.cl>
2021-10-18 19:18:17 -0300

    Se agrega typeahead_substring_matcher

--- extensions/sowerphp/app/webroot/js/app.js
+++ extensions/sowerphp/app/webroot/js/app.js
@@ -130,3 +130,17 @@ function dataTableNum(data, type, full) {
     }
     return data;
 }
+
+function typeahead_substring_matcher(strs) {
+    return function findMatches(q, cb) {
+        var matches, substringRegex;
+        matches = [];
+        substrRegex = new RegExp(q, 'i');
+        $.each(strs, function(i, str) {
+            if (substrRegex.test(str)) {
+                matches.push(str);
+            }
+        });
+        cb(matches);
+    };
+}
