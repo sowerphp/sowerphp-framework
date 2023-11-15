9206761bc103988f00c7e00f12e7be475a924031
Esteban De La Fuente Rubio <esteban@delaf.cl>
2023-11-15 02:05:51 -0300

    fix doc

--- extensions/sowerphp/app/Utility/Rut.php
+++ extensions/sowerphp/app/Utility/Rut.php
@@ -33,7 +33,8 @@ class Utility_Rut
 
     /**
      * Método que valida el RUT ingresado
-     * @param rut RUT con dígito verificador (puntos son opcionales)
+     * @param mixed $rut RUT con dígito verificador (puntos son opcionales)
+     * @return bool
      * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
      * @version 2019-07-04
      */
@@ -52,8 +53,8 @@ class Utility_Rut
 
     /**
      * Calcula el dígito verificador de un RUT
-     * @param r RUT al que se calculará el dígito verificador
-     * @return Dígito verificar
+     * @param mixed $r RUT al que se calculará el dígito verificador
+     * @return string Dígito verificador
      * @author Desconocido
      * @version 2010-05-23
      */
@@ -62,33 +63,36 @@ class Utility_Rut
         $r = str_replace('.', '', $r);
         $r = str_replace(',', '', $r);
         $s=1;
-        for ($m=0;$r!=0;$r/=10)
-            $s=($s+$r%10*(9-$m++%6))%11;
-        return strtoupper(chr($s?$s+47:75));
+        for ($m = 0; $r != 0; $r/=10) {
+            $s = ($s + $r%10 * (9 - $m++ % 6)) % 11;
+        }
+        return strtoupper(chr($s ? $s + 47 : 75));
     }
 
     /**
      * Transforma un RUT a un formato con sólo los números o formateado,
      * dependerá de como sea pasado el RUT
-     * @param rut RUT que se quiere transformar
-     * @param quitarDV Si es true el dígito verificador se quita, sino se mantiene
-     * @return RUT formateado
+     * @param mixed $rut RUT que se quiere transformar
+     * @param bool $quitarDV Si es true el dígito verificador se quita, sino se mantiene
+     * @return mixed RUT formateado
      * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
      * @version 2014-11-01
      */
     public static function normalizar($rut, $quitarDV = true)
     {
-        if (is_array($rut)) return self::normalizar_array($rut, $quitarDV);
+        if (is_array($rut)) {
+            return self::normalizar_array($rut, $quitarDV);
+        }
         return self::normalizar_string($rut, $quitarDV);
     }
 
     private static function normalizar_array($arreglo, $quitarDV = true)
     {
         if (isset($arreglo[1]) and (!empty($arreglo[1]) or $arreglo[1]==='0')) {
-            $arreglo[0] .= '-'.strtoupper($arreglo[1]);
+            $arreglo[0] .= '-' . strtoupper($arreglo[1]);
         }
         if (!strpos($arreglo[0], '-')) {
-            $arreglo[0] = substr($arreglo[0], 0, -1).'-'.substr($arreglo[0], -1);
+            $arreglo[0] = substr($arreglo[0], 0, -1) . '-' . substr($arreglo[0], -1);
         }
         $rut = self::normalizar_string($arreglo[0]);
         $dv = substr($arreglo[0], -1);
@@ -97,31 +101,32 @@ class Utility_Rut
 
     private static function normalizar_string($rut, $quitarDV = true)
     {
-        if (!isset($rut[0]))
+        if (!isset($rut[0])) {
             return '';
+        }
         $rut = strtoupper(str_replace(['.', ','], '', $rut));
         if (strpos($rut, '-')) {
-            if($quitarDV) {
+            if ($quitarDV) {
                 $aux = explode('-', $rut);
                 return (int)array_shift($aux);
             }
             return (int)str_replace('-', '', $rut);
         } else {
             $rutNew = number_format((int)substr($rut, 0, -1), 0, '', '.');
-            return $rutNew.'-'.$rut[strlen($rut)-1];
+            return $rutNew . '-' . $rut[strlen($rut)-1];
         }
     }
 
     /**
      * Agrega un número su dígito verificador y lo formatea como RUT
-     * @param rut RUT en formato número y sin dígito verificador
-     * @return RUT formateado
+     * @param mixed $rut RUT en formato número y sin dígito verificador
+     * @return string RUT formateado
      * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
      * @version 2014-11-01
      */
     public static function addDV($rut)
     {
-        return self::normalizar_string($rut.self::dv($rut));
+        return self::normalizar_string($rut . self::dv($rut));
     }
 
 }
