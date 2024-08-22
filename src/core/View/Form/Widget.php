7567d5b4766de4e6604c1b7513ada4fcd88e54d2
Kalcio <matiasmunoz.me@gmail.com>
2024-08-22 16:08:19 -0400

    Se migran los widgets de los formularios a excepción de los que funcionan con JS. Se añade documentación a los widgets.

--- src/core/View/Form/Widget.php
+++ src/core/View/Form/Widget.php
@@ -49,7 +49,7 @@ class View_Form_Widget implements \ArrayAccess
      * Los atributos (u opciones) del widget del formulario.
      *
      * Normalmente serán atributos directos de un elemento de formmulario HTML.
-     * Sin embargo, algunos widgets usan opciones más complehas para rederizar
+     * Sin embargo, algunos widgets usan opciones más complejas para rederizar
      * los elementos (ej: un widget con múltiples elementos HTML).
      *
      * @var array
@@ -132,6 +132,11 @@ class View_Form_Widget implements \ArrayAccess
         return $this->$method($options);
     }
 
+    /**
+     * Renderiza el widget predeterminado input del campo del formulario.
+     *
+     * @return string El HTML renderizado del widget.
+     */
     protected function renderDefaultWidget(): string
     {
         $this->attributes['value'] = $this->attributes['value'] ?? $this->value;
@@ -141,11 +146,21 @@ class View_Form_Widget implements \ArrayAccess
         );
     }
 
+    /**
+     * Renderiza el widget de tipo text.
+     *
+     * @return string El HTML con el campo text renderizado.
+     */
     protected function renderTextWidget(): string
     {
         return $this->renderDefaultWidget();
     }
 
+    /**
+     * Renderiza el widget de tipo date.
+     *
+     * @return string El HTML con el campo date renderizado.
+     */
     protected function renderDateWidget(): string
     {
         if (!empty($this->value)) {
@@ -155,6 +170,11 @@ class View_Form_Widget implements \ArrayAccess
         return $this->renderDefaultWidget();
     }
 
+    /**
+     * Renderiza el widget de tipo DatetimeLocal.
+     *
+     * @return string El HTML renderizado del widget.
+     */
     protected function renderDatetimeLocalWidget(): string
     {
         if (!empty($this->value)) {
@@ -164,6 +184,11 @@ class View_Form_Widget implements \ArrayAccess
         return $this->renderDefaultWidget();
     }
 
+    /**
+     * Renderiza el widget de tipo Textarea.
+     *
+     * @return string El HTML renderizado del widget.
+     */
     protected function renderTextareaWidget(): string
     {
         return sprintf(
@@ -173,76 +198,262 @@ class View_Form_Widget implements \ArrayAccess
         );
     }
 
+    /**
+     * Renderiza el widget de tipo div.
+     *
+     * @return string El HTML renderizado del widget.
+     */
     protected function renderDivWidget(): string
     {
-        return '';
+        return sprintf(
+            '<div %s>%s</div>',
+            html_attributes($this->attributes),
+            $this->value
+        );
     }
 
+    /**
+     * Renderiza el widget de tipo hidden.
+     *
+     * @return string El HTML con el campo Hidden renderizado.
+     */
     protected function renderHiddenWidget(): string
     {
-        return '';
+        $this->attributes['type'] = 'hidden';
+        return $this->renderDefaultWidget();
     }
 
+    /**
+     * Renderiza el widget de tipo button.
+     *
+     * @return string El HTML con el campo del Button renderizado.
+     */
     protected function renderButtonWidget(): string
     {
-        return '';
+        // En caso de no tener tipo por defecto se asigna tipo button.
+        $this->attributes['type'] = $this->attributes['type'] ?? 'button';
+        return sprintf(
+            '<button %s>%s</button>',
+            html_attributes($this->attributes),
+            $this->value
+        );
     }
 
+    /**
+     * Renderiza el widget de tipo password.
+     *
+     * @return string El HTML con el campo Password renderizado.
+     */
     protected function renderPasswordWidget(): string
     {
-        return '';
+        $this->attributes['type'] = 'password';
+        return sprintf(
+            '<div class="input-group">%s%s%s</div>',
+            $this->renderDefaultWidget(),
+            '<a class="input-group-text" style="cursor:pointer;text-decoration:none" onclick="Form.showPassword(this)"><i class="fa-regular fa-eye fa-fw"></i></a>',
+            '<a class="input-group-text" style="cursor:pointer;text-decoration:none" onclick="__.copy(this.parentNode.querySelector(\'input\').value, \'Copiado.\')"><i class="fa-regular fa-copy fa-fw"></i></a>'
+        );
     }
 
+    /**
+     * Renderiza el widget de tipo bool.
+     *
+     * @return string El HTML con el campo Bool renderizado.
+     */
     protected function renderBoolWidget(): string
     {
-        return '';
+        // Si no se proporciona $this->value, se utiliza por defecto ['No', 'Yes']
+        $options = isset($this->value) && is_array($this->value) ? $this->value : ['No', 'Yes'];
+        $html = '';
+        // Asegura que $options sea un array con exactamente dos elementos
+        if (count($options) === 2) {
+            foreach ($options as $key => $option) {
+                // Verifica que $option sea un string
+                if (is_string($option)) {
+                    // Determina cuál opción debería estar seleccionada
+                    $selected = !empty($this->attributes['value']) && $this->attributes['value'] === $option ? ' selected' : '';
+                    $html .= sprintf(
+                        '<option value="%s"%s>%s</option>',
+                        $key,
+                        $selected,
+                        htmlspecialchars($option)
+                    );
+                }
+            }
+        } else {
+            throw new \Exception("Expected exactly 2 options for the boolean widget.");
+        }
+        return sprintf(
+            '<select %s>%s</select>',
+            html_attributes($this->attributes),
+            $html
+        );
     }
 
+    /**
+     * Renderiza el widget de tipo select.
+     *
+     * @return string El HTML con el campo Select renderizado.
+     */
     protected function renderSelectWidget(): string
     {
-        return '';
+        $html = '';
+        $this->value = is_array($this->value) ? $this->value : [];
+        if (!empty($this->value)) {
+            foreach ($this->value as $option) {
+                if (isset($option['value'], $option['label']) && is_string($option['value']) && is_string($option['label'])) {
+                    // Verifica si la opción debe estar seleccionada, basada en la clave 'selected'
+                    $selected = !empty($option['selected']) ? ' selected' : '';
+                    $html .= sprintf(
+                        '<option value="%s" %s>%s</option>',
+                        htmlspecialchars($option['value']),
+                        $selected,
+                        htmlspecialchars($option['label'])
+                    );
+                }
+            }
+        }
+        return sprintf(
+            '<select %s>%s</select>',
+            html_attributes($this->attributes),
+            $html
+        );
     }
 
+
+    /**
+     * Renderiza el widget de tipo file.
+     *
+     * @return string El HTML con el campo File renderizado.
+     */
     protected function renderFileWidget(): string
     {
-        return '';
+        $this->attributes['type'] = 'file';
+        return $this->renderDefaultWidget();
     }
 
+    /**
+     * Renderiza el widget de tipo files.
+     *
+     * @return string El HTML renderizado del widget.
+     */
     protected function renderFilesWidget(): string
     {
+
         return '';
     }
 
+    /**
+     * Renderiza el widget de tipo table.
+     *
+     * @return string El HTML renderizado del widget.
+     */
     protected function renderTableWidget(): string
     {
         return '';
     }
 
+    /**
+     * Renderiza el widget de js table.
+     *
+     * @return string El HTML renderizado del widget.
+     */
     protected function renderJsWidget(): string
     {
         return '';
     }
 
+    /**
+     * Renderiza el widget de check table.
+     *
+     * @return string El HTML con el campo Checkbox renderizado.
+     */
     protected function renderCheckboxWidget(): string
     {
-        return '';
+        $this->attributes['type'] = 'checkbox';
+        $this->attributes['value'] = $this->attributes['value'] ?? $this->value;
+        return sprintf(
+            '<input %s/> %s',
+            html_attributes($this->attributes),
+            htmlspecialchars($this->attributes['value']),
+        );
     }
 
+    /**
+     * Renderiza el widget de checkboxes table.
+     *
+     * @return string El HTML con el campo Checkboxes múltiples renderizado.
+     */
     protected function renderCheckboxesWidget(): string
     {
-        return '';
+        $html = '';
+        $this->value = is_array($this->value) ? $this->value : [];
+
+        if (!empty($this->value)) {
+            foreach ($this->value as $index => $option) {
+                if (isset($option['value'], $option['label']) && is_string($option['value']) && is_string($option['label'])) {
+                    $checked = !empty($option['checked']) ? ' checked' : '';
+                    $id = htmlspecialchars($option['id'] ?? $this->name . '_' . $index);
+                    $html .= sprintf(
+                        '<input type="checkbox" id="%s" name="%s[]" value="%s" %s %s> <label>%s</label><br>',
+                        $id,
+                        htmlspecialchars($this->name),
+                        htmlspecialchars($option['value']),
+                        html_attributes($this->attributes),
+                        $checked,
+                        htmlspecialchars($option['label'])
+                    );
+                }
+            }
+        }
+        return sprintf('<div>%s</div>', $html);
     }
 
+    /**
+     * Renderiza el widget de tablecheck table.
+     *
+     * @return string El HTML renderizado del widget.
+     */
     protected function renderTableCheckWidget(): string
     {
         return '';
     }
 
-    protected function renderRadiosWidget(): string
+    /**
+     * Renderiza el widget de radio.
+     *
+     * @return string El HTML con el campo Radio renderizado.
+     */
+    protected function renderRadioWidget(): string
     {
-        return '';
+        {
+            $html = '';
+            $this->value = is_array($this->value) ? $this->value : [];
+            if (!empty($this->value)) {
+                foreach ($this->value as $index => $option) {
+                    if (isset($option['value'], $option['label']) && is_string($option['value']) && is_string($option['label'])) {
+                        $checked = !empty($option['checked']) ? ' checked' : '';
+                        $id = htmlspecialchars($option['id'] ?? $this->name . '_' . $index);
+                        $html .= sprintf(
+                            '<input type="radio" id="%s" name="%s" value="%s"%s> <label>%s</label><br>',
+                            $id,
+                            htmlspecialchars($this->name),
+                            htmlspecialchars($option['value']),
+                            $checked,
+                            htmlspecialchars($option['label'])
+                        );
+                    }
+                }
+            }
+            return sprintf('<div>%s</div>', $html);
+        }
     }
 
+    /**
+     * Renderiza el widget de tableradios.
+     *
+     * @return string El HTML renderizado del widget.
+     */
     protected function renderTableRadiosWidget(): string
     {
         return '';
